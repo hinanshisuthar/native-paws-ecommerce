9db8feef4978405706b6311e6d8233ace6db0379
Hinanshi Suthar <hinanshis4@gmail.com>
2022-03-21 02:53:59 +0530

    Feat: added React-Router

--- src/index.js
+++ src/index.js
@@ -3,13 +3,18 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
-
+import {BrowserRouter as Router} from 'react-router-dom'
+import { ProductProvider } from "./utilities/ProductContext";
 // Call make Server
 makeServer();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Router>
+      <ProductProvider>
+        <App />
+      </ProductProvider>
+    </Router>
+  </React.StrictMode>,  
   document.getElementById("root")
 );
