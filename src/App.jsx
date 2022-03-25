d1314c87d9e20c309f645b28934624abb2665cb9
Hinanshi Suthar <hinanshis4@gmail.com>
2022-03-25 21:11:01 +0530

    update: added routes for auth pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,11 @@ import "./App.css";
 import { Routes, Route } from 'react-router-dom'
 import { HomePage } from "./pages/home/HomePage";
 import { ProductPage } from './pages/ProductPage/ProductPage'
-import {WishlistPage} from './pages/Wishlist Page/WishlistPage'
-import {CartPage} from './pages/Cart Page/CartPage'
+import { WishlistPage } from './pages/Wishlist Page/WishlistPage'
+import { CartPage } from './pages/Cart Page/CartPage'
+import { SignUp } from "./pages/Auth Pages/SignUp";
+import { LogIn } from "./pages/Auth Pages/LogIn";
+import { LogOut } from "./pages/Auth Pages/LogOut";
 
 function App() {
   return (
@@ -13,6 +16,9 @@ function App() {
         <Route path="/products" element={<ProductPage />}/>
         <Route path="/wishlist" element={<WishlistPage />}/>
         <Route path="/cart" element={<CartPage />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/log-in" element={<LogIn />} />
+        <Route path="/log-out" element={<LogOut />} />
       </Routes>
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,111 +4,157 @@ import { useCart } from "../context/cart-context";
 import { useWishlist } from "../context/wishlist-context";
 
 const Navbar = () => {
-    const [show, toggleShow] = useState(false);
-    const [showHamburgerMenu, toggleHamburgerMenu] = useState(false);
-    const {wishlistState} = useWishlist();
-    const {cartState} = useCart();
+  const [show, toggleShow] = useState(false);
+  const [showHamburgerMenu, toggleHamburgerMenu] = useState(false);
+  const { wishlistState } = useWishlist();
+  const { cartState } = useCart();
 
-    const showOptions = () => {
-        toggleShow(!show)
-    }
+  const showOptions = () => {
+    toggleShow(!show);
+  };
 
-    const hamburgerMenu = () => {
-        toggleHamburgerMenu(!showHamburgerMenu)
-    }
+  const hamburgerMenu = () => {
+    toggleHamburgerMenu(!showHamburgerMenu);
+  };
 
-    return (
-        <>
-        <header className="page-header flex-col-sb">
-            <div className="header-content flex-row-sb p-1">
-                <Link to="/" className="logo">
-                    <img src="https://res.cloudinary.com/do5ez3xws/image/upload/v1647500585/home/logo_bbmksz.webp" alt="logo" className="min-img img-responsive" />
+  return (
+    <>
+      <header className="page-header flex-col-sb">
+        <div className="header-content flex-row-sb p-1">
+          <Link to="/" className="logo">
+            <img
+              src="https://res.cloudinary.com/do5ez3xws/image/upload/v1647500585/home/logo_bbmksz.webp"
+              alt="logo"
+              className="min-img img-responsive"
+            />
+          </Link>
+          <div className="search-con flex-row-sb img-responsive">
+            <form action="#" className="form-search flex-row-sb">
+              <input
+                type="text"
+                className="text-rg pl-1"
+                id="search-field"
+                placeholder="Search the entire store..."
+              />
+              <button type="submit" className="btn-icon pr-sm">
+                <i className="fa-solid fa-magnifying-glass search-icon thin"></i>
+              </button>
+            </form>
+          </div>
+          <div className="flex-row-sb right-nav px-sm">
+            <div className="shop-con flex-row-sb">
+              <Link to="/products" className="link">
+                <h5>Shop All Products</h5>
+              </Link>
+              <i className="fa-solid fa-angle-down"></i>
+            </div>
+            <div className="experience-con flex-row-sb">
+              <Link className="link" to="/">
+                <h5>Offers</h5>
+              </Link>
+            </div>
+            <div className="location-con">
+              <div className="location flex-row-sb">
+                <Link className="link" to="/">
+                  <h5>Location</h5>
                 </Link>
-                <div className="search-con flex-row-sb img-responsive">
-                    <form action="#" className="form-search flex-row-sb">
-                        <input type="text" className="text-rg pl-1" id="search-field" placeholder="Search the entire store..." />
-                        <button type="submit" className="btn-icon pr-sm"><i
-                                className="fa-solid fa-magnifying-glass search-icon thin"></i></button>
-                    </form>
-                </div>
-                <div className="flex-row-sb right-nav px-sm">
-                    <div className="shop-con flex-row-sb">
-                        <Link to="/products" className="link">
-                            <h5>Shop All Products</h5>
-                        </Link>
-                        <i className="fa-solid fa-angle-down"></i>
-                    </div>
-                    <div className="experience-con flex-row-sb">
-                        <a className="link" href="#">
-                            <h5>Offers</h5>
-                        </a>
-                    </div>
-                    <div className="location-con">
-                        <div className="location flex-row-sb">
-                            <a className="link" href="#">
-                                <h5>Location</h5>
-                            </a>
-                            <i className="fa-solid fa-angle-down"></i>
-                        </div>
-                    </div>
-                    <div className="dropdown account-con">
-                        <button  onClick={showOptions}><i className="fa-regular fa-user text-md dropbtn"></i></button>
-                        {show && <div id="myDropdown" className="dropdown-content" style={{display: 'block'}}>
-                            <a href="/screens/sign-up.html" className="p-1">Sign Up</a>
-                            <a href="/screens/log-in.html" className="p-1">Log In</a>
-                            <a href="/screens/user-profile.html" className="p-1">My Account</a>
-                        </div> }
-                    </div>
-                    <div className="wish-con">
-                        <Link to="/wishlist">
-                            <span className="badge-icon badge-container">
-                                <i className="fas fa-heart badge-icon-base text-md"></i>
-                                    {wishlistState.wishlist.length === 0 ? (
-                                        <span></span>) : <span className="count-badge heart-badge-number">
-                                            {wishlistState.wishlist.length}
-                                                         </span>
-                                    }
-                            </span>
-                        </Link>
-                    </div>
-                    <div className="cart-con">
-                        <Link to="/cart">
-                            <span className="badge-icon badge-container">
-                                <i className="fa-solid fa-cart-shopping text-md"></i>
-                                    {cartState.cart.length === 0 ? (
-                                        <span></span>) : <span className="count-badge heart-badge-number">
-                                            {cartState.cart.length}
-                                                         </span>
-                                    }
-                                    
-                            </span>
-                        </Link>
-                    </div>
-                </div>
-                <div className="bars-con" onClick={hamburgerMenu}>
-                    <div className="user">
-                        <i className="fas fa-bars"></i>
-                    </div>
-                </div>
+                <i className="fa-solid fa-angle-down"></i>
+              </div>
             </div>
-            <div className="search-btm p-1">
-                <form action="#" className="form-search flex-row-sb">
-                    <input type="text" className="text-rg pl-1" id="search-field" placeholder="search the entire store..." />
-                    <button type="submit" className="btn-icon pr-sm"><i
-                            className="fa-solid fa-magnifying-glass search-icon thin"></i></button>
-                </form>
+            <div className="dropdown account-con">
+              <button onClick={showOptions}>
+                <i className="fa-regular fa-user text-md dropbtn"></i>
+              </button>
+              {show && (
+                <div
+                  id="myDropdown"
+                  className="dropdown-content"
+                  style={{ display: "block" }}>
+                  <Link to="/sign-up" className="p-1">
+                    Sign Up
+                  </Link>
+                  <Link to="/log-in" className="p-1">
+                    Log In
+                  </Link>
+                  <Link to="/log-out" className="p-1">
+                    Log Out
+                  </Link>
+                </div>
+              )}
             </div>
-            {showHamburgerMenu && <div className="menu p-sm" style={{display: 'block'}}>
-                <h5><a href="/screens/product-page.html">Shop All Products</a></h5>
-                <h5><a href="/screens/wishlist-page.html">My Wishlist</a></h5>
-                <h5><a href="/screens/cart-page.html">My Shopping Cart</a></h5>
-                <h5><a href="">Location</a></h5>
-                <h5><a href="/screens/sign-up.html">Sign Up</a></h5>
-                <h5><a href="/screens/log-in.html">Log In</a></h5>
-            </div> }
-        </header>
-        </>
-    )
-}
+            <div className="wish-con">
+              <Link to="/wishlist">
+                <span className="badge-icon badge-container">
+                  <i className="fas fa-heart badge-icon-base text-md"></i>
+                  {wishlistState.wishlist.length === 0 ? (
+                    <span></span>
+                  ) : (
+                    <span className="count-badge heart-badge-number">
+                      {wishlistState.wishlist.length}
+                    </span>
+                  )}
+                </span>
+              </Link>
+            </div>
+            <div className="cart-con">
+              <Link to="/cart">
+                <span className="badge-icon badge-container">
+                  <i className="fa-solid fa-cart-shopping text-md"></i>
+                  {cartState.cart.length === 0 ? (
+                    <span></span>
+                  ) : (
+                    <span className="count-badge heart-badge-number">
+                      {cartState.cart.length}
+                    </span>
+                  )}
+                </span>
+              </Link>
+            </div>
+          </div>
+          <div className="bars-con" onClick={hamburgerMenu}>
+            <div className="user">
+              <i className="fas fa-bars"></i>
+            </div>
+          </div>
+        </div>
+        <div className="search-btm p-1">
+          <form action="#" className="form-search flex-row-sb">
+            <input
+              type="text"
+              className="text-rg pl-1"
+              id="search-field"
+              placeholder="search the entire store..."
+            />
+            <button type="submit" className="btn-icon pr-sm">
+              <i className="fa-solid fa-magnifying-glass search-icon thin"></i>
+            </button>
+          </form>
+        </div>
+        {showHamburgerMenu && (
+          <div className="menu p-sm" style={{ display: "block" }}>
+            <h5>
+              <Link to="/products">Shop All Products</Link>
+            </h5>
+            <h5>
+              <Link to="/wishlist">My Wishlist</Link>
+            </h5>
+            <h5>
+              <Link to="/cart">My Shopping Cart</Link>
+            </h5>
+            <h5>
+              <Link to="/">Location</Link>
+            </h5>
+            <h5>
+              <Link to="/sign-up">Sign Up</Link>
+            </h5>
+            <h5>
+              <Link to="/log-in">Log In</Link>
+            </h5>
+          </div>
+        )}
+      </header>
+    </>
+  );
+};
 
-export {Navbar};
+export { Navbar };
