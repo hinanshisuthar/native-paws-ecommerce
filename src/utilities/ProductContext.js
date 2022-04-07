6cb406bd22c2aaaa0e5a79acf8fa201c1df1fd59
Hinanshi Suthar <hinanshis4@gmail.com>
2022-04-07 15:36:49 +0530

    fix: indentation

--- src/utilities/ProductContext.js
+++ src/utilities/ProductContext.js
@@ -1,18 +1,33 @@
-import { createContext, useContext, useReducer } from "react";
+import { createContext, useContext, useReducer, useState } from "react";
 import { reducerFunction } from "./reducerFunction";
 
 const ProductContext = createContext();
 const useProduct = () => useContext(ProductContext);
 
-const ProductProvider = ({children}) => {
-    const [state, dispatch] = useReducer(reducerFunction, {
-        sortBy: "",
-        categories: {food: false, leash: false, toys: false, clothes: false, small: false, medium: false, large: false, arrival: false, choice: false, bestseller: false},
-        price: 500,
-    })
-    return (
-        <ProductContext.Provider value={{state, dispatch}}>{children}</ProductContext.Provider>
-    )
-}
+const ProductProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducerFunction, {
+    sortBy: "",
+    categories: {
+      food: false,
+      leash: false,
+      toys: false,
+      clothes: false,
+      small: false,
+      medium: false,
+      large: false,
+      arrival: false,
+      choice: false,
+      bestseller: false,
+    },
+    price: 500,
+  });
+  const [search, setSearch] = useState("");
 
-export {useProduct, ProductProvider};
+  return (
+    <ProductContext.Provider value={{ state, dispatch, search, setSearch }}>
+      {children}
+    </ProductContext.Provider>
+  );
+};
+
+export { useProduct, ProductProvider };
