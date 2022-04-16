4f2346009252fba5d28c09c3acf72681389f706d
Hinanshi Suthar <hinanshis4@gmail.com>
2022-04-17 01:43:19 +0530

    feat: 404 Page created

---
+++ src/pages/404-page/404Page.jsx
@@ -0,0 +1,12 @@
+import './404Page.css'
+
+const _404Page = () => {
+    return (
+        <div className="error-img-container center py-1">
+            <img src="https://res.cloudinary.com/do5ez3xws/image/upload/v1650139072/home/The_page_you_were_trying_is_not_found_sg3hxu.svg" alt="404-image" />
+        </div>
+    )
+}
+
+export {_404Page};
+
