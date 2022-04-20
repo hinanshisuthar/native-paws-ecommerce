16779551aa9e9d3914550f0db728fa2ad1cf9844
Hinanshi Suthar <hinanshis4@gmail.com>
2022-04-20 20:13:10 +0530

    feat: added provider for auth

--- src/index.js
+++ src/index.js
@@ -3,15 +3,17 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
-import {BrowserRouter as Router} from 'react-router-dom'
+import { BrowserRouter as Router } from "react-router-dom";
 import { ProductProvider } from "./utilities/ProductContext";
 import { WishlistProvider } from "./context/wishlist-context";
 import { CartProvider } from "./context/cart-context";
+import { AuthProvider } from "./utilities/context/auth-context";
 // Call make Server
 makeServer();
 
 ReactDOM.render(
   <React.StrictMode>
+    <AuthProvider>
       <ProductProvider>
         <WishlistProvider>
           <CartProvider>
@@ -21,6 +23,7 @@ ReactDOM.render(
           </CartProvider>
         </WishlistProvider>
       </ProductProvider>
-  </React.StrictMode>,  
+    </AuthProvider>
+  </React.StrictMode>,
   document.getElementById("root")
 );
